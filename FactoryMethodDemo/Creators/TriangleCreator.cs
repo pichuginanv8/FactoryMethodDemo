d2f5a95e55847d6b5fcce555ebc4cf33b21e2cc5
Пичугина Нина Владимировна <PichuginaNV@dom201.local>
2026-02-26 14:10:06 +0700

    check

--- FactoryMethodDemo/Creators/TriangleCreator.cs
+++ FactoryMethodDemo/Creators/TriangleCreator.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 using FactoryMethodDemo.Models;
 
+//привет
 namespace FactoryMethodDemo.Creators
 {
     public abstract class TriangleCreator
